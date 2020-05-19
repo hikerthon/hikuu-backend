d3f830378c7b577ba8d074578c707d7599f26837
Nyotie <garmseven@gmail.com>
2020-05-19 23:36:10 +0800

    fix alerts route

--- src/alerts/alerts.controller.ts
+++ src/alerts/alerts.controller.ts
@@ -1,47 +1,6 @@
-import { ApiResponse, ApiPropertyOptional, ApiProperty, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
-import { AlertsService } from "./alerts.service";
-
-export class Alerts {
-    @ApiProperty()
-    permit_id: number;
-  
-    @ApiProperty()
-    permit_name: string;
-  
-    @ApiProperty()
-    location: string;
-    
-    @ApiProperty()
-    radius: number;
-
-    @ApiProperty()
-    alert_id: number;
-
-    @ApiProperty()
-    alert_level: string;
-
-    @ApiPropertyOptional()
-    event_id: number;
-
-    @ApiProperty()
-    event_type: string;
-
-    @ApiProperty()
-    event_info: string;
-
-    @ApiProperty()
-    event_time: string;
-
-    @ApiProperty()
-    ttl: number;
-    
-    @ApiProperty()
-    station_id: number;
-  
-    @ApiProperty()
-    station_name: string;
-}
+import { AlertsService, Alerts } from "./alerts.service";
 
 @ApiTags('alerts')
 @Controller('alerts')
@@ -51,5 +10,6 @@ export class AlertsController {
     @Get()
     @ApiResponse({ status: 200, type: Alerts, isArray: true, description: 'Return list of alert' })
     getAllEvent() {
-        return this.alertSvc.getAllAlerts();
-    }}
+        return this.alertSvc.getFakeAlerts();
+    }
+}
--- src/alerts/alerts.service.ts
+++ src/alerts/alerts.service.ts
@@ -1,8 +1,74 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Injectable } from '@nestjs/common';
+import { stat } from 'fs';
+
+export class Alerts {
+    @ApiProperty()
+    permitId: number;
+  
+    @ApiProperty()
+    permitName: string;
+  
+    @ApiProperty()
+    location: string;
+    
+    @ApiProperty()
+    radius: number;
+
+    @ApiProperty()
+    alertId: number;
+
+    @ApiProperty()
+    alertLevel: string;
+
+    @ApiProperty()
+    eventId: number;
+
+    @ApiProperty()
+    eventType: string;
+
+    @ApiProperty()
+    eventInfo: string;
+
+    @ApiProperty()
+    eventTime: string;
+
+    @ApiProperty()
+    ttl: number;
+    
+    @ApiProperty()
+    stationId: number;
+  
+    @ApiProperty()
+    stationName: string;
+
+    constructor(permitId: number, permitName: string, location: string, radius: number, 
+        alertId: number, alertLevel: string, eventId: number, eventType: string, 
+        eventInfo: string, eventTime: string, ttl: number, stationId: number, stationName: string){
+        this.permitId = permitId;
+        this.permitName = permitName;
+        this.location = location;
+        this.radius = radius;
+        this.alertId = alertId;
+        this.alertLevel = alertLevel;
+        this.eventId = eventId;
+        this.eventType = eventType;
+        this.eventInfo = eventInfo;
+        this.eventTime = eventTime;
+        this.ttl = ttl;
+        this.stationId = stationId;
+        this.stationName = stationName;
+    }
+}
 
 @Injectable()
 export class AlertsService {
-    getAllAlerts() {
-        return [{}]
+    alerts = [
+        new Alerts(1, 'Yushan National Park Permit', '23.468818, 120.954489', 3, 2, 'Caution', 1, 'Animal', 'Water Buffalloo nearby', '2020-05-19 17:00', 6, 1, 'Yushan'),
+        new Alerts(2, 'Taroko National Park Permit', '24.174251, 121.565319', 5, 3, 'Danger', 1, 'Animal', 'Cobra spotted nearby', '2020-05-19 17:00', 6, 1, 'Taroko')
+    ]
+
+    getFakeAlerts() {
+        return this.alerts
     }
 }
