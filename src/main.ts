dc1f7cd9dc741a0ef65848c77f02981fda1ef02d
sivan <sivantist@gmail.com>
2020-05-21 13:06:31 +0800

    create mobile api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { MobileappModule } from './mobile/mobileapp.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const mobileApp = await NestFactory.create(MobileappModule);
 
   const options = new DocumentBuilder()
     .setTitle('Hikoo')
@@ -14,6 +16,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
+  const mOptions = new DocumentBuilder()
+    .setTitle('Hikoo Mobile')
+    .setDescription('Hikoo Mobile API')
+    .setVersion('1.0')
+    .addTag('hikoo')
+    .build();
+  const mDocument = SwaggerModule.createDocument(mobileApp, mOptions);
+  SwaggerModule.setup('api', mobileApp, mDocument);
+
   await app.listen(3000);
+  await mobileApp.listen(3001)
 }
 bootstrap();
