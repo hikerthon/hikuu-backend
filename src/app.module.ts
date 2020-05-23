edf704e998da330e84f691a6786085e9b9d0aff6
Tony Lin <tony.lin@ardomusnet.com>
2020-05-23 17:49:09 +0800

    feat(example): add fcm push notification example usage

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { EventsGateway } from './web/events/events.gateway';
+import { FirebaseAdminModule } from '@aginix/nestjs-firebase-admin';
+import * as admin from 'firebase-admin';
+import { readFileSync } from 'fs';
 
 @Module({
   imports: [
+    FirebaseAdminModule.forRootAsync({
+      useFactory: () => {
+
+        const hikooService = JSON.parse(
+          readFileSync(join(__dirname, '../fcm/hikoo.json')).toString()
+        );
+
+        return {
+          credential: admin.credential.cert(hikooService),
+          databaseURL: 'https://bamboo-creek-277702.firebaseio.com'
+        };
+      }
+    }),
     TypeOrmModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'static/socketio'),
