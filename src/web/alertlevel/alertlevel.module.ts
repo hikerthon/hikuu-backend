bcffca39f9497e705a89d69f247df20f29e81a9d
NasinLin <sheng41020@gmail.com>
2020-05-24 08:20:31 +0800

    Hot fix missing ormmodule

--- src/web/alertlevel/alertlevel.module.ts
+++ src/web/alertlevel/alertlevel.module.ts
@@ -1,8 +1,11 @@
 import { Logger, Module } from '@nestjs/common';
 import { AlertlevelService } from './alertlevel.service';
 import { AlertlevelController } from './alertlevel.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AlertlevelEntity } from '../../share/entity/alertlevel.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AlertlevelEntity])],
   providers: [Logger, AlertlevelService],
   controllers: [AlertlevelController]
 })
--- src/web/permit/permit.module.ts
+++ src/web/permit/permit.module.ts
@@ -1,8 +1,11 @@
 import { Module, Logger } from '@nestjs/common';
 import { PermitController } from './permit.controller';
 import { PermitService } from './permit.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PermitEntity } from '../../share/entity/permit.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PermitEntity])],
   controllers: [PermitController],
   providers: [Logger, PermitService]
 })
